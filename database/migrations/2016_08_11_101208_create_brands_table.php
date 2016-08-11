5045e14b5a36d4d916e37a52fe998a66ed3cc709
amosjuw <amosjuw@gmail.com>
2016-08-11 15:11:37 +0800

    php  artisan   seeds

---
+++ database/migrations/2016_08_11_101208_create_brands_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+//        Schema::create('brands', function (Blueprint $table) {
+//            $table->increments('id');
+//            $table->string('name',60);
+//            $table->string('created_at_ip');
+//            $table->string('updated_at_ip');
+//            $table->date('created_at');
+//            $table->date('updated_at');
+//            $table->timestamps();
+//        });
+        Schema::create('brands', function (Blueprint $table) {
+                        $table->increments('id');
+            $table->string("name")->unique();
+            $table->string("created_at_ip");
+            $table->string("updated_at_ip");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('brands');
+    }
+}
